411b3e2463bbbc55225795f4a78a3d635482cb16
Ryan Gilbert <ryan.gilbert@formidable.com>
2019-10-21 15:30:49 -0600

    Simplify demo

--- demo/src/Thread.js
+++ demo/src/Thread.js
@@ -1,27 +1,22 @@
 import React from 'react';
-import { useMutation } from 'urql';
 
 import Heart from './Heart';
 
 const Thread = ({ id, title, likesNumber }) => {
-  // write likeThread mutation
-  const [response, likeThread] = useMutation(`
-    mutation LikeThread($threadId: ID!) {
-      likeThread(threadId: $threadId) {
-        id
-      }
-    } 
-  `);
+  // 1. write likeThread mutation
 
-  if (response.error) {
-    alert(response.error);
-  }
+  // 2. handle error from response
+
+  const handleHeartClick = () => {
+    // 3. fire off the likeThread mutation
+  };
 
   return (
     <div className="Thread">
       <span>{title}</span>
-      <button className="Heart" onClick={() => likeThread({ threadId: id })}>
-        <Heart liked={response.data != null} />
+      <button className="Heart" onClick={handleHeartClick}>
+        {/* 4. Use response.error to determine if mutation was successful */}
+        <Heart liked={false} />
       </button>
       <span className="LikesNumber">{likesNumber}</span>
     </div>
--- demo/src/Threads.js
+++ demo/src/Threads.js
@@ -1,32 +1,12 @@
 import React from 'react';
-import { useQuery } from 'urql';
 
 import Thread from './Thread';
 
 const Threads = () => {
-  // fetch threads
-  const [response] = useQuery({
-    query: `
-      {
-        threads(sortBy: LATEST) {
-          id
-          title
-          likesNumber
-        }
-      } 
-    `,
-  });
+  // 1. write query to fetch threads
 
-  if (response.fetching) return <div>Fetching threads...</div>;
-  if (response.error) return <div>Error fetching threads :(</div>;
-
-  return (
-    <div>
-      {response.data.threads.map(t => (
-        <Thread key={t.id} {...t} />
-      ))}
-    </div>
-  );
+  // 2. render threads from query response
+  return <div></div>;
 };
 
 export default Threads;
